6268f242f093dba0eb3aea9893d622193daca56c
Jonathan Kruse <jonathanskruse@gmail.com>
2018-09-04 14:52:12 -0400

    search

---
+++ server/routes/dbMainSearch.router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+// const request = require("request");
+
+
+
+    router.post('/', (req, res) => {
+        let columName = req.body.colum
+        let queryVar = req.body.query
+        let queryText = `SELECT * FROM inspections
+        WHERE ${columName} LIKE '%${queryVar}%'`;
+        pool.query(queryText)
+         .then((result)=>{
+        res.sendStatus(201);
+        }).catch((err)=>{
+        console.log(err);
+        res.sendStatus(500)
+        })
+})
+
+       
+    module.exports = router;
